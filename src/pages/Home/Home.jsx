363e354a014a6d14e7b6592ad81d98ae0f4c75cb
Karina-98 <115569415+Karina-98@users.noreply.github.com>
2023-11-26 18:23:20 +0100

    +

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,54 @@
 
 import { HomeDiv, Text, TextH1 } from "./Home.styled";
 
+
+
+const expenses = {
+    "2023-01": {
+        "01": {
+            "food": [ 22.11, 43, 11.72, 2.2, 36.29, 2.5, 19 ],
+            "fuel": [ 210.22 ]
+        },
+        "09": {
+            "food": [ 11.9 ],
+            "fuel": [ 190.22 ]
+        },
+    },
+    "2023-03": {
+        "07": {
+            "food": [ 20, 11.9, 30.20, 11.9 ]
+        },
+        "04": {
+            "food": [ 10.20, 11.50, 2.5 ],
+            "fuel": []
+        }
+    },
+    "2023-04": {}
+}
+function get_median_of_first_week_expenses(expenses) {
+  let result = {};
+  let allExpenses = [];
+  for (let year in expenses) {
+    for (let day in expenses[year]) {
+      for (let category in expenses[year][day]) {
+        allExpenses = allExpenses.concat(expenses[year][day][category])
+      }
+      allExpenses.sort((a, b) => a - b)
+      if (allExpenses.length) {
+        const mid = Math.floor(allExpenses.length / 2);
+        result = (allExpenses.length % 2 !== 0)
+          ? Math.round(allExpenses[mid])
+          : Math.round((allExpenses[mid - 1] + result[mid]) / 2);
+      } else {
+        result[year] = null;
+      }
+    }
+  }
+    return result;
+  }
+
+console.log(get_median_of_first_week_expenses(expenses))
+
 export default function Home() {
   return (<div>
     <HomeDiv  >
